4947f4ff04da9eee8935d38a185e230a3adfb5f7
Hyunjun Choi <snowandice21@nethru.co.kr>
2022-12-19 15:37:15 +0900

    first commit

---
+++ src/main/java/com/pp/rrr/ppstreamtest/PpStreamTestApplication.java
@@ -0,0 +1,13 @@
+package com.pp.rrr.ppstreamtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpStreamTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpStreamTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pp/rrr/ppstreamtest/PpStreamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.rrr.ppstreamtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpStreamTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
