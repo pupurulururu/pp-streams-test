d2c014a5a5a12ffc80e9e48f80c38db7e39423ef
Hyunjun Choi <snowandice21@nethru.co.kr>
2022-12-20 14:03:38 +0900

    그룹해서숫자세본거

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
   - s1:u1
 - reset
 - sh kafka-streams-application-reset.sh --bootstrap-servers localhost:9092 --application-id pp-streams --input-topics topic1 --to-earliest
-
+- 조회
+- sh kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic1 --from-beginning --property print.key=true --property key.separator=":"
--- src/main/java/com/pp/rrr/ppstreamtest/PpStreamTestApplication.java
+++ src/main/java/com/pp/rrr/ppstreamtest/PpStreamTestApplication.java
@@ -4,10 +4,9 @@ import com.pp.rrr.ppstreamtest.model.Event;
 import com.pp.rrr.ppstreamtest.serde.EventSerdes;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.processor.StateStore;
+import org.apache.kafka.streams.state.StoreBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
 import java.util.Properties;
 
 @SpringBootApplication
@@ -33,7 +33,7 @@ public class PpStreamTestApplication implements ApplicationRunner {
 
         Properties properties = getProperties();
 
-        Topology topology = table_print();
+        Topology topology = stream_print2();
 
         KafkaStreams streams = new KafkaStreams(topology, properties);
 
@@ -49,7 +49,7 @@ public class PpStreamTestApplication implements ApplicationRunner {
         return properties;
     }
 
-    public Topology table_print() {
+    public Topology stream_print() {
 
         StreamsBuilder builder = new StreamsBuilder();
 
@@ -57,10 +57,40 @@ public class PpStreamTestApplication implements ApplicationRunner {
                 (sid,uid) -> KeyValue.pair(sid, StringUtils.isEmpty(uid) ? "unknown_"+sid : uid));
 
         stream.print(Printed.toSysOut());
-        stream.toTable().toStream().print(Printed.toSysOut());
+        KTable<String, String> stringStringKTable = stream.toTable();
+        stringStringKTable.toStream().print(Printed.toSysOut());
 
 
         return builder.build();
     }
+
+    //stream -> table ->steam.print()랑 똑같다
+    public Topology simple_table_tostream_print() {
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        builder.table(appConfig.getKafkaInputTopic(), Consumed.with(Serdes.String(), Serdes.String())).toStream().print(Printed.toSysOut());
+
+        return builder.build();
+    }
+
+
+    /*
+    [KTABLE-TOSTREAM-0000000007]: s1, 4
+    [KTABLE-TOSTREAM-0000000007]: s2, 3
+     */
+    public Topology stream_print2() {
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KGroupedStream<String, String> groupByKey = builder.stream(appConfig.getKafkaInputTopic(), Consumed.with(Serdes.String(), Serdes.String())).map(
+                (sid, uid) -> KeyValue.pair(sid, StringUtils.isEmpty(uid) ? "unknown_" + sid : uid)).groupByKey();
+
+        groupByKey.count().toStream().print(Printed.toSysOut());
+
+        return builder.build();
+    }
+
+
 
 }
